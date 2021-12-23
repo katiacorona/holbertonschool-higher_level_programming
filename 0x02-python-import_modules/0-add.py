aa7e4de9d354c9129a4582c0a81984a84f1e5354
katiacorona <katiacorona@gmail.com>
2021-12-23 20:30:36 +0000

    executed as if it was imported

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+
+""" Imports function add and prints the result of 1 + 2 """
+if __name__ == "__main__":
+
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
