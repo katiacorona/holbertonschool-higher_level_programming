6f3348d7f26206ebed2e21b89ebd32a13a4fe927
katiacorona <katiacorona@gmail.com>
2021-12-24 05:08:35 +0000

    First test

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" Prints the number of and the list of its arguments """
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    args = len(sys.argv) - 1
+    if args != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    op = {'+': add, '-': sub, '*': mul, '/': div}
+    if sys.argv[2] not in list(op.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, op[sys.argv[2]](a, b)))
