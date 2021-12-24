7e9d11aac0cec646503ecda523d2e0a5b7864a0a
katiacorona <katiacorona@gmail.com>
2021-12-24 00:51:19 +0000

    First test

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" Prints the number of and the list of its arguments """
+if __name__ == "__main__":
+    import sys
+    args = len(sys.argv) - 1
+    if args == 0:
+        print("{} arguments.".format(args))
+    elif args == 1:
+        print("{} argument:".format(args))
+    else:
+        print("{} arguments:".format(args))
+    for i in range (1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
