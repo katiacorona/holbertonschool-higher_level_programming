2ec83784dfa13f3d7a44a2a8527ffdd20535df2c
katiacorona <katiacorona@gmail.com>
2021-12-24 00:04:29 +0000

    removes __name__ from the comments

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """ Prints the results of each function in calculator_1 """
+    """ Prints the the sum, difference, product, and quotient for 10 and 5 """
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
