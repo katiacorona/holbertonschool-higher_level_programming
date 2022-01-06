6b897ccd4cc375b84514c6a976ff165ff060218e
katiacorona <katiacorona@gmail.com>
2022-01-06 04:18:46 +0000

    Second test

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for elem in set_1:
-        if elem in set_2:
-            return set(elem)
+    return list(set(set_1).intersection(set_2))
