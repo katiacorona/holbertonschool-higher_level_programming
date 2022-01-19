72c7643965deff5b5a1ea28e58f275132658513e
katiacorona <katiacorona@gmail.com>
2022-01-19 01:44:15 +0000

    Fix:
    removes if condition preventing 0 from being evaluated as an integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -6,9 +6,9 @@ def safe_print_integer(value):
 
     Returns:
     True if it's an integer; False otherwise. """
-    if value:
-        try:
-            print("{:d}".format(value))
-            return True
-        except (ValueError, TypeError):
-            return False
+
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
