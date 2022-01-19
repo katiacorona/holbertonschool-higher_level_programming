0b3f9ebb0f15fe1259140b16309d19ef19443475
katiacorona <katiacorona@gmail.com>
2022-01-19 01:39:29 +0000

    Includes:
    exception error for TypeError preventing values with symbols being evaluated

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -6,9 +6,9 @@ def safe_print_integer(value):
 
     Returns:
     True if it's an integer; False otherwise. """
-
-    try:
-        print("{:d}".format(value))
-        return True
-    except ValueError:
-        return False
+    if value:
+        try:
+            print("{:d}".format(value))
+            return True
+        except (ValueError, TypeError):
+            return False
