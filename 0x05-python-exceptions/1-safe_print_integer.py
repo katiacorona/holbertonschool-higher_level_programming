943c7a3e65c5588698c10af219892881d7661039
katiacorona <katiacorona@gmail.com>
2022-01-19 01:34:25 +0000

    Includes:
    function definition to print an integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """ Prints an integer with "{:d}.format()
+    Parameters:
+    value (int): the value to be printed.
+
+    Returns:
+    True if it's an integer; False otherwise. """
+
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False
