9de70b9cee555cf08bcea5eca234beba87458e54
katiacorona <katiacorona@gmail.com>
2022-04-28 21:32:22 +0000

    Includes: solution for cases when the cycle does not start in the head node

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,12 +11,16 @@ int check_cycle(listint_t *list)
 	listint_t *current, *temp;
 
 	current = list;
-	temp = current->next;
-	while (temp->next != NULL)
+	while (current->next != NULL)
 	{
-		if (temp->next == current)
-			return (1);
-		temp = temp->next;
+		temp = current->next;
+		while (temp->next != NULL)
+		{
+			if (temp->next == current)
+				return (1);
+			temp = temp->next;
+		}
+		current = current->next;
 	}
 	return (0);
 }
