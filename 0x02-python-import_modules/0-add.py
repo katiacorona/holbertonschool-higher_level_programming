a365de22c394967bdda30481a370e47bfe92494b
katiacorona <katiacorona@gmail.com>
2022-05-02 22:12:38 +0000

    Uses f string formatting and declarations in same line

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-
-""" Imports function add and prints the result of 1 + 2 """
+""" Imports function add and prints the results of a + b"""
 if __name__ == "__main__":
-
     from add_0 import add
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    a, b = 1, 2
+    print(f"{a} + {b} = {add(a, b)}")
