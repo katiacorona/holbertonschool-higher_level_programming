27b7395e638b90eea8505e32641e68eb4389475a
katiacorona <katiacorona@gmail.com>
2022-05-04 23:35:22 +0000

    Cleaned code

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    i = 0
-
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+    for n in my_list:
+        print(n)
