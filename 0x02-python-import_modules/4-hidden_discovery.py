3d49cdf4379cd9e765cb454ae18b7546dfd986da
katiacorona <katiacorona@gmail.com>
2022-05-04 18:54:07 +0000

    Cleanes code

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-""" Prints the number of and the list of its arguments """
+""" Prints the number of and the list of the imported module's arguments """
 if __name__ == "__main__":
     import hidden_4
-
-    lst = (dir(hidden_4))
-    for name in lst:
+    ls = dir(hidden_4)
+    for name in ls:
         if name[:2] != '__':
             print(name)
