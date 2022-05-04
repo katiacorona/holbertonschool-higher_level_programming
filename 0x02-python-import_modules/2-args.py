1a8c0222d325b17fd6d6b13b541ed8cc1fd38044
katiacorona <katiacorona@gmail.com>
2022-05-04 00:50:47 +0000

    Imports only argv for cleaner code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
+from sys import argv
 
 """ Prints the number of and the list of its arguments """
 if __name__ == "__main__":
-    import sys
-    args = len(sys.argv) - 1
-    if args == 0:
-        print("{} arguments.".format(args))
-    elif args == 1:
-        print("{} argument:".format(args))
+    argc = len(argv) - 1
+    if argc == 0:
+        print("{} arguments.".format(argc))
     else:
-        print("{} arguments:".format(args))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+        print("{} argument:".format(argc))
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, argv[i]))
