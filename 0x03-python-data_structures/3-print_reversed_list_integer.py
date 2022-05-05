31550ee9087cbaa60e846b626c798b817115e28d
katiacorona <katiacorona@gmail.com>
2022-05-05 02:51:45 +0000

    Removes: blank line with whitespaces

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,7 @@
 def print_reversed_list_integer(my_list=[]):
     """
     Print all integers of a list, in reverse order.
-    
+
     Format: one integer per line. The list only contains integers.
     """
     if my_list:
