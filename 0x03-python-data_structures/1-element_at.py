9ff11d178203d80b578749f74bd43bd3f6420ca6
katiacorona <katiacorona@gmail.com>
2022-05-05 01:20:01 +0000

    Cleans code

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx > (len(my_list) - 1):
+    """
+    Retrieves an element from a list, like in C.
+
+    Return: None if idx < 0 or if idx is out of range.
+    """
+    if 0 > idx > (len(my_list) - 1):
         return None
     return (my_list[idx])
