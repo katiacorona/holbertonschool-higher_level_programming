672544ac2ceb67de6de878b58092a9cf76f0446c
katiacorona <katiacorona@gmail.com>
2022-05-19 02:16:51 +0000

    Task 6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    """
+    Print a dictionary by ordered keys.
+    Description: All keys are strings. Keys should be sorted by alphabetic or-
+    der. Only sort keys of the first level (not from a dictionary insed the main
+    dictionary).
+    """
+    for k in sorted(a_dictionary.keys()):
+        print("{:s}: {}".format(k, a_dictionary[k]))
