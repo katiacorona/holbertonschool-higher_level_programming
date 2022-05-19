9e4c255f717afdd654090d838c00b5d24dad12aa
katiacorona <katiacorona@gmail.com>
2022-05-19 01:40:17 +0000

    Fixed errors

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,5 +3,4 @@ def common_elements(set_1, set_2):
     """
     Return a set of common elements in two sets.
     """
-    if set_1 and set_2:
-        return (set_1 & set_2)
+    return (set_1 & set_2)
