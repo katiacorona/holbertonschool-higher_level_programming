fd1caec2bcf6790c3f05db9e373ceeff14ee264e
katiacorona <katiacorona@gmail.com>
2022-05-19 02:17:59 +0000

    Fixes: pycodestyle

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,8 +3,8 @@ def print_sorted_dictionary(a_dictionary):
     """
     Print a dictionary by ordered keys.
     Description: All keys are strings. Keys should be sorted by alphabetic or-
-    der. Only sort keys of the first level (not from a dictionary insed the main
-    dictionary).
+    der. Only sort keys of the first level (not from a dictionary insed the
+    main dictionary).
     """
     for k in sorted(a_dictionary.keys()):
         print("{:s}: {}".format(k, a_dictionary[k]))
