b96f5a3cfabe1b07ec8ba9f55840b737b3542a70
katiacorona <katiacorona@gmail.com>
2022-05-20 17:58:08 +0000

    Fixes: symbol[i] < symbol[i + 1]

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,24 +4,30 @@ def roman_to_int(roman_string):
     Convert a Roman numeral to an integer.
     Return: An integer. If the roman_string is not a string or None, return 0.
     """
-    roman_dic = {
-                "I": 1,
-                "V": 5,
-                "X": 10,
-                "L": 50,
-                "C": 100,
-                "D": 500,
-                "M": 1000
-              }
+    roman_dict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+    }
     n = 0
 
-    if roman_string is None:
-        return 0
+    if (roman_string is None or not isinstance(roman_string, str)):
+        return None
 
     for idx in range(len(roman_string)):
-        if roman_dic[roman_string[idx]] < roman_dic[roman_string[idx]]:
-            n -= roman_dic[roman_string[idx]]
+        if idx == len(roman_string) - 1:
+            n += roman_dict[roman_string[idx]]
+            break
+
+        if (roman_dict[roman_string[idx]] < roman_dict[roman_string[idx + 1]]
+                and idx != len(roman_string) - 1):
+            n -= roman_dict[roman_string[idx]]
+
         else:
-            n += roman_dic[roman_string[idx]]
+            n += roman_dict[roman_string[idx]]
 
     return n
