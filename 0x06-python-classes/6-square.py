a620fa13627645d20da98d7f09d9f62ffabbe4c2
katiacorona <katiacorona@gmail.com>
2022-05-25 05:11:47 +0000

    Attempt no 1029121590

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -69,8 +69,8 @@ class Square:
             print("")
             return
 
-        [print("") for i in range(0, self.__position[1])]
-        for j in range(0, self.__size):
-            [print(" ", end="") for k in range(0, self.__position[0])]
-            [print("#", end="") for l in range(0, self.__size)]
+        [print("") for i in range(self.__position[1])]
+        for i in range(self.__size):
+            [print(" ", end="") for j in range(self.__position[0])]
+            [print("#", end="") for k in range(self.__size)]
             print("")
