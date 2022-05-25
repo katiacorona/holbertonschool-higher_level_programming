7434328c2e586ae42e63f22b8503ee7bfbfae24e
katiacorona <katiacorona@gmail.com>
2022-05-25 05:03:27 +0000

    Adds: return for size == 0

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -65,10 +65,12 @@ class Square:
 
     def my_print(self):
         """Prints in stdout the square with the character #"""
-        [print("") for n in range(self.__position[1])]
-        for i in range(self.__size):
-            [print(" ", end="") for s in range(self.__position[0])]
-            [print("#", end="") for j in range(self.__size)]
-            print("")
         if self.__size == 0:
+            print("")
+            return
+
+        [print("") for i in range(self.__position[1])]
+        for i in range(self.__size):
+            [print(" ", end="") for j in range(self.__position[0])]
+            [print("#", end="") for k in range(self.__size)]
             print("")
