76c2f03c92ffaba14de9ed84c0fe13f4bd224795
katiacorona <katiacorona@gmail.com>
2022-06-01 00:26:09 +0000

    Includes: dif size rows case

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -30,5 +30,7 @@ def matrix_divided(matrix, div):
                     for x in [num for row in matrix for num in row])):
         raise TypeError('matrix must be a matrix (list of lists)'
                         ' of integers/floats')
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError('Each row of the matrix must have the same size')
 
     return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
