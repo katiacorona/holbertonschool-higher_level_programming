b95805c949d4902ea4d5409a6df011fe379a5c06
katiacorona <katiacorona@gmail.com>
2022-06-03 17:44:01 +0000

    Fixes: ValueError Documentation, adds: task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -45,7 +45,7 @@ class Rectangle:
         """Property setter: sets the width of a rectangle.
 
         Raises:
-            TypeError: if height is not an int.
+            TypeError: if height is not an int
             ValueError: if height < 0.
         """
         if not isinstance(value, int):
@@ -53,3 +53,13 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """Returns the rectangle area."""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Returns the rectangle perimeter."""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return (self.__width * 2 + self.__height * 2)
