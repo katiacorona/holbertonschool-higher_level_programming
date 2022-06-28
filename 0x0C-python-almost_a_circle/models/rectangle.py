aaca097993d26010eab661da75932f56a94dd163
katiacorona <katiacorona@gmail.com>
2022-06-28 02:29:44 +0000

    Task 4: updates Rectangle with public method area

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -74,3 +74,7 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """Returns the Rectangle area."""
+        return (self.__width * self.__height)
