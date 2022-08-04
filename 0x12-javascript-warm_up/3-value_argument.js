b4a2ec4cef1280777edd04bc6146fce4d7b525bc
katiacorona <katiacorona@gmail.com>
2022-08-04 03:29:01 +0000

    Third task

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+'use strict';
+
+const args = process.argv.slice(2);
+const len = args.length;
+
+if (len < 1) {
+  console.log('No argument');
+} else {
+  console.log(args[0]);
+}
