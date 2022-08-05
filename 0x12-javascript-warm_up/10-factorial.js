d20fb3b96a9c1ddb0f85dca03af241d2ad8d4977
katiacorona <katiacorona@gmail.com>
2022-08-05 03:08:09 +0000

    Reduces cases to positive numbers

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -5,10 +5,10 @@ const n = parseInt(process.argv.slice(2)[0]);
 console.log(factorial(n));
 
 function factorial (n) {
-  if (n <= 1 || Number.isNaN(n)) {
+  if (n === 0 || isNaN(n)) {
     return 1;
   }
-  const res = factorial(n - 1) * n;
+  const res = n * factorial(n - 1);
 
   return res;
 }
