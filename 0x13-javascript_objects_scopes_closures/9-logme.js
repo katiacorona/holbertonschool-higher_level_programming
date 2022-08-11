0c8008a0cde4ec33c44928ab671001a41251005f
katiacorona <katiacorona@gmail.com>
2022-08-11 07:20:23 +0000

    Include task 9 global scope var

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+let count = 0;
+
+exports.logMe = function (item) {
+  console.log(`${count}: ${item}`);
+  count++;
+};
+/*
+const logMe = require('./9-logme').logMe;
+
+logMe("Hello");
+logMe("Best");
+logMe("School");
+*/
