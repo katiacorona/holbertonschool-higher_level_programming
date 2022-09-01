18d5d2d221bd8caef2ddcd97362b37f5a7f79a8a
katiacorona <katiacorona@gmail.com>
2022-09-01 20:48:17 +0000

    Fixes check for same class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def is_same_class(obj, a_class):
-    if (isinstance(obj, a_class) and (type(obj) is a_class)):
+    if type(obj) is a_class:
         return True
     return False
